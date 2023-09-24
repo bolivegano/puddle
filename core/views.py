fd33cc1c30d8e24bba7104c2a32b6e4a26b697f5
Bolivegano <bolivegano@proton.me>
2023-09-23 20:32:44 -0600

    up to 1:10:00 in video tutorial

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 # Create your views here.
 
@@ -13,4 +15,18 @@ def index(request):
     
 
 def contact(request):
-    return render(request, 'core/contact.html')    
+    return render(request, 'core/contact.html')   
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('/login')
+    else:
+        form = SignupForm() 
+    
+    context = {'form': form,}
+    return render(request, 'core/signup.html', context)
