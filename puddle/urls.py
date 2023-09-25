ffa6766d2c016333344d3e533cf2498eef056a18
Bolivegano <bolivegano@proton.me>
2023-09-25 17:33:15 -0600

    up to 1:47:00 in video

--- puddle/urls.py
+++ puddle/urls.py
@@ -18,10 +18,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import index, contact
-
 urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
